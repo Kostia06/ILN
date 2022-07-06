bd7c15c8492c757f00f087d97c41569db22180bc
Kostia Ilnytskyi <78446034+Kostia06@users.noreply.github.com>
2022-07-06 11:24:38 -0400

    Delete KOS directory

--- KOS/Operator/check.lua
+++
@@ -1,7 +0,0 @@
-function Operator:Check(txt, line, block)
-    if inList(txt, operators) then
-        for _,i in ipairs(split(txt,';')) do
-            self:New(i, line, block)
-        end
-    end
-end
